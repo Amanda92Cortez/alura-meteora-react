5297ef9567454291a6429dc08ec5ee2d33590625
Amanda92Cortez <amanda_cortez_2012@hotmail.com>
2025-04-11 14:58:38 -0300

    Ajuste

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/alura-meteora-react/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
